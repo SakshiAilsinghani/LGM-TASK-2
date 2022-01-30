3ee41bc044bb94baf53e71fd463ee3fce9225cf1
SakshiAilsinghani <96494342+SakshiAilsinghani@users.noreply.github.com>
2022-01-30 19:34:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style.css";
+import App from "./App";
+import webvitals from "./webvitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
